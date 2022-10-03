cc985dc4b9a663bbf2bad9f1a07c8431806ac7d6
[carlos martinez] <theycallhellp@gmail.com>
2022-10-02 21:29:24 -0700

    add pseudo code for js

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// declare variables of
+    // array of question objects [{question (string), choices (array of string), which index of array is correct}, ...]
+    // choice buttons - selects all choice buttons
+var choiceButtons = document.querySelectorAll(".choice");
+// start button begins timer
+    // with timer started, hide the quiz-begin div and show the quiz-going div
+    // replace elements in quiz-begun div with first question
+// when one of the buttons is clicked, check to see if the button clicked was the right answer
+// console.log(choiceButtons);
